d000bcc89522f487b98233e184c7fc224fc3d7d4
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-16 10:51:33 +0100

    setup testing_recap as pub_tests

--- inheritance/inheritance_start_point/classes/motorbike.py
+++ inheritance/inheritance_start_point/classes/motorbike.py
@@ -1,6 +1,6 @@
 from classes.vehicle import Vehicle
 
-class Motorbike(Vehicle):
+class Motorbike(Vehicle):       #<Motorbike>(<inherits from Vehicle>)
     def start_engine(self):
         return "BBBRRRRMMMMM"
 
