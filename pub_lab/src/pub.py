f27bbc7035f95b7d55e40fe41227302de84b7ca3
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-16 12:03:53 +0100

    Started setup of pub lab by initialising required files.

---
+++ pub_lab/src/pub.py
@@ -0,0 +1,9 @@
+from Drinks import *
+from Customer import *
+
+
+class Pub:
+    def __init__(self, name, cash_in_till):
+        self.name = name
+        self.cash = cash_in_till  
+
---
+++ pub_lab/tests/pub_test.py
@@ -0,0 +1,12 @@
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("Ox", 100.00)        #Pub("Ox", 100.00) must have capital P for Pub because its the class Pub.
+
+    def test_pub_has_name(self):
+        self.assertEqual("Ox", self.pub.name)
+
+    
+
